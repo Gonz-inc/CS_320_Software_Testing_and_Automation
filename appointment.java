af276065df265507bd73724256ceef5e90a11f13
Gerardo Gonzalez <gerardo.gonzalez3@snhu.edu>
2023-07-27 06:16:12 -0400

    First Commit

---
+++ appointment.java
@@ -0,0 +1,18 @@
+/*
+ * Appointment class that contains an appointment ID, and appointment date field and 
+ * appointment descriptions. All of these are methods. 
+ * Institution: SNHU 
+ * Class: CS-320 
+ * Created by Gerardo Gonzalez. 
+ * Date: 2023-27-7 5:54 AM EST
+ * 5-1 Milestone: Appointment Service
+ * */
+package appointmentService;
+
+public class appointment{
+	
+	//Default Constructor. 
+	public appointment() {
+		
+	}
+}
